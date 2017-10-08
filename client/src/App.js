168f1ba2e8f7df0932acb85115dc19733d7f3ff6
adamzaczek <adamzaczekul@gmail.com>
2017-10-09 00:24:03 +0200

    Mock Network Connection, Fix Dependency Mismatch, Make Some Coffe

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,60 @@
 import React, { Component } from 'react';
+import {
+  ApolloClient,
+  gql,
+  graphql,
+  ApolloProvider,
+} from 'react-apollo';
+import { 
+  makeExecutableSchema,
+  addMockFunctionsToSchema
+} from 'graphql-tools';
+ import { mockNetworkInterfaceWithSchema } from 'apollo-test-utils';
+ import { typeDefs } from './schema';
 import logo from './logo.svg';
 import './App.css';
 
-const ChannelsList = () =>
-    (<ul className="item-list">
-      <li>Channel 1</li>
-      <li>Channel 2</li>
-    </ul>);
+const schema = makeExecutableSchema({ typeDefs });
+addMockFunctionsToSchema({ schema });
+const mockNetworkInterface = mockNetworkInterfaceWithSchema({ schema });
+const client = new ApolloClient({
+  networkInterface: mockNetworkInterface,
+});
+const channelsListQuery = gql`
+query ChannelsListQuery {
+  channels {
+    id
+    name
+  }
+}
+`;
+
+const ChannelsList = ({ data: {loading, error, channels }}) => {
+  if (loading) {
+    return <div>Loading ...</div>;
+  }
+  if (error) {
+    return <div>{error.message}</div>;
+  }
+  return <ul className="item-list">
+    { channels.map( channel => <li key={channel.id}>{channel.name}</li> ) }
+  </ul>;
+};
+
+const ChannelsListWithData = graphql(channelsListQuery)(ChannelsList);
 
 class App extends Component {
   render() {
     return (
-      <div className="app">
-        <div className="app-header">
-          <img src={logo} className="app-logo" alt="logo" />
-          <h2>Chat App</h2>
+      <ApolloProvider client={client}>
+        <div className="app">
+          <div className="app-header">
+            <img src={logo} className="app-logo" alt="logo" />
+            <h2>Chat App</h2>
+          </div>
+          <ChannelsListWithData />
         </div>
-        <ChannelsList />
-      </div>
+      </ApolloProvider>
     );
   }
 }
