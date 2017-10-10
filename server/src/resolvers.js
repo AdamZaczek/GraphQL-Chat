2a02c0c9696274554fded1cc8a2878557330dc5f
adamzaczek <adamzaczekul@gmail.com>
2017-10-10 22:41:43 +0200

    Add First Mutation

--- server/src/resolvers.js
+++ server/src/resolvers.js
@@ -12,10 +12,20 @@ const channels = [
   }
 ]
 
+// I really need to decide which database to use, this is madness
+let nextId = 3
+
 export const resolvers = {
   Query: {
     channels: () => {
       return channels
+    }
+  },
+  Mutation: {
+    addChannel: (root, args) => {
+      const newChannel = { id: nextId++, name: args.name }
+      channels.push(newChannel)
+      return newChannel
     }
   }
 }
--- server/src/schema.js
+++ server/src/schema.js
@@ -14,6 +14,12 @@ type Channel {
 type Query {
   channels: [Channel]    # "[]" means this is a list of channels
 }
+
+# The mutation root type, used to define all mutations.
+type Mutation {
+  # A mutation to add a new channel to the list of channels
+  addChannel(name: String!): Channel
+}
 `
 
 const schema = makeExecutableSchema({ typeDefs, resolvers })
